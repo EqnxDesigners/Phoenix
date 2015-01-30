3929cc897c287e1550e8a84bdf8f7821afe8509e
Jerome Clerc <jclerc@eqnx.ch>
2015-01-30 17:05:59 +0100

    FontEnd2

---
+++ app/includes/footer.inc.php
@@ -0,0 +1,67 @@
+<!-- FOOTER -->
+<footer class="wide-row" id="index-footer">
+    <div class="row wide-row" id="google-map">
+        GOOGLE MAP
+    </div>
+    <div class="row">
+        <div class="small-12 columns">
+            <h3>Nous contacter</h3>
+        </div>
+        <div class="small-6 columns">
+            <div class="row coord-info">
+                <div class="small-1 columns">
+                    <span class="icon icon-shape-localisation icon-param-color-white"></span>
+                </div>
+                <div class="small-11 columns">
+                    Equinoxe MIS Development<br>
+                    InnovationPark EPFL (bât. IPE-C)<br>
+                    Route Cantonale<br>
+                    CH-1015 Lausanne
+                </div>
+            </div>
+            <div class="row coord-info">
+                <div class="small-1 columns">
+                    <span class="icon icon-shape-paperplane icon-param-color-white"></span>
+                </div>
+                <div class="small-11 columns">
+                    <a href="mailto:info@eqnx.ch">info@eqnx.ch</a>
+                </div>
+            </div>
+            <div class="row coord-info">
+                <div class="small-1 columns">
+                    <span class="icon icon-shape-telephone icon-param-color-white"></span>
+                </div>
+                <div class="small-11 columns">
+                    +41 21 693 89 38
+                </div>
+            </div>
+        </div>
+        <div class="small-6 columns">
+            <form action="">
+                <div class="small-12 columns">
+                    <div class="group">
+                        <input type="text" name="nom" required />
+                        <label>Nom prénom</label>
+                    </div>
+                </div>
+                <div class="small-12 columns">
+                    <div class="group">
+                        <input type="text" name="email" required />
+                        <label>E-mail</label>
+                    </div>
+                </div>
+                <div class="small-12 columns">
+                    <textarea name="message" id="" cols="30" rows="8" placeholder="Message"></textarea>
+                </div>
+                <div class="small-12 columns text-right">
+                    <input type="submit" name="send-form" value="Envoyer" class="button" />
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="row">
+        <div class="small-12 columns text-center copyright">
+            &copy;&nbsp;Equinoxe MIS Development | 2015
+        </div>
+    </div>
+</footer>
