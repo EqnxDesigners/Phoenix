a530e8bdbd77f3bc8519322b6a3985887e18d993
Jerome Clerc <jclerc@eqnx.ch>
2015-04-07 17:03:32 +0200

    intégration textes

--- app/includes/qui.inc.php
+++ app/includes/qui.inc.php
@@ -12,8 +12,37 @@
     <hr/>
 </section>
 <section class="row sub-page">
-    <h3><?php getTexte('qui', 'txt2_title'); ?></h3>
-    <div class="row">
-        <?php getMultiLineTexte('qui', 'txt2_para', 'text-justify'); ?>
+    <div class="small-12 medium-4 columns text-center eqnx-sectors">
+        <div class="small-12 columns">
+            IMAGE
+        </div>
+        <div class="small-12 columns">
+            <h4><?php getTexte('qui', 'txt2_title'); ?></h4>
+        </div>
+        <div class="small-12 columns">
+            <?php getMultiLineTexte('qui', 'txt2_para', 'text-justify'); ?>
+        </div>
+    </div>
+    <div class="small-12 medium-4 columns text-center eqnx-sectors">
+        <div class="small-12 columns">
+            IMAGE
+        </div>
+        <div class="small-12 columns">
+            <h4><?php getTexte('qui', 'txt3_title'); ?></h4>
+        </div>
+        <div class="small-12 columns">
+            <?php getMultiLineTexte('qui', 'txt3_para', 'text-justify'); ?>
+        </div>
+    </div>
+    <div class="small-12 medium-4 columns text-center eqnx-sectors">
+        <div class="small-12 columns">
+            IMAGE
+        </div>
+        <div class="small-12 columns">
+            <h4><?php getTexte('qui', 'txt4_title'); ?></h4>
+        </div>
+        <div class="small-12 columns">
+            <?php getMultiLineTexte('qui', 'txt4_para', 'text-justify'); ?>
+        </div>
     </div>
 </section>
